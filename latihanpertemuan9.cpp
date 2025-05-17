9917ff1877195f0debce0bcd82fad2a7911342ca
Hafidh014 <muhammad.hafidh2006@gmail.com>
2025-05-17 15:36:42 +0700

    membuat class admin dan petugas dan di bikin public dan private

--- latihanpertemuan9.cpp
+++ latihanpertemuan9.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Petugas;
+class Admin;
+
+class Buku {
+private:
+    string judul, penulis;
+    bool dipinjam;
+
+public:
+    Buku(string j, string p) : judul(j), penulis(p), dipinjam(false) {}
+
+    void info() {
+        cout << "Judul: " << judul << " | Penulis: " << penulis
+             << " | Status: " << (dipinjam ? "Dipinjam" : "Tersedia") << endl;
+    }
+     friend class Petugas;
+    friend class Admin;
+};
